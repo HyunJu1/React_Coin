73da96884b8a7b0c8da7b6ff23e992dda7158355
HyunJu1 <hh960305@naver.com>
2018-05-03 12:32:26 +0900

    testing

--- src/containers/coinIcon.js
+++ src/containers/coinIcon.js
@@ -5,7 +5,8 @@ const CoinIcon = props => {
   let symbol = props.coinSymbol;
   let url = `https://chasing-coins.com/api/v1/std/logo/${symbol}`;
   return (
-    <img src={url} alt={`${symbol} logo`} />
+    <img src={url} 
+      alt={`${symbol} logo`} />
   );
 };
 
