590b82af577d213f9fa80912c0a36ffb1994936b
HyunJu1 <hh960305@naver.com>
2018-05-07 02:55:10 +0900

    selecting chart interval

--- src/components/coinGraph.js
+++ src/components/coinGraph.js
@@ -3,19 +3,44 @@ import _ from 'lodash';
 import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 import axios from 'axios';
 import moment from 'moment';
-// import {fetchGraph} from '../actions';
-
+import '../index.css';
 class CoinGraph extends Component{
   constructor(props){
     super(props);
     this.state={
-      coinList:[]
+      coinList:[],
+      interval:'7d',
     };
   }
+  handleClick(buttonId) {
+    this.setState({
+      interval:buttonId
+    });
+
+
+  }
+
  
   render(){
     let now = Date.now();
-    let then = now - 24 * 60 * 60 * 1000 * 7; //7DAY
+    var then;
+    //var then = now - 24 * 60 * 60 * 1000 * 7; //7DAY
+    if(this.state.interval=='7d'){
+      then = now - 24 * 60 * 60 * 1000 * 7; //7DAY
+    }else if(this.state.interval=='1d'){
+      then = now - 24 * 60 * 60 * 1000 ; //1DAY
+    }
+    else if(this.state.interval=='1m'){
+      then = now - 24 * 60 * 60 * 1000 *30; //1m
+    }else if(this.state.interval=='3m'){
+      then = now - 24 * 60 * 60 * 1000 *90; //3m
+    }
+    else if(this.state.interval=='1y'){
+      then = now - 24 * 60 * 60 * 1000 *365; //1y
+    }
+
+
+    console.log(moment(then).format("YYYY/MM/DD/HH:MM"));
     let id = this.props.id;
     let url = `https://graphs2.coinmarketcap.com/currencies/${id}/${then}/${now}/`;
     axios.get(url).then(response=>{ 
@@ -38,17 +63,25 @@ class CoinGraph extends Component{
     );
       
     return(
-  
-      <LineChart width={1100} height={500} data={chartData1}>
-        <XAxis dataKey='idd'/>
-        <YAxis dataKey='price' fill='#ffffff'/>
-        <CartesianGrid strokeDasharray="4 4"/>
-        <Tooltip/>
-        <Legend />
-        <Line dataKey='price'  fill= '#ffffff'  >
-        </Line>
-      </LineChart>    
-
+      <div className="chart">
+        <p>Selected : {this.state.interval} </p>
+        <LineChart width={1100} height={500} data={chartData1}>
+          <XAxis dataKey='idd'/>
+          <YAxis dataKey='price' fill='#ffffff'/>
+          <CartesianGrid strokeDasharray="4 4"/>
+          <Tooltip/>
+          <Legend />
+          <Line dataKey='price'  fill= '#ffffff'  >
+          </Line>
+        </LineChart>    
+        <button className="btn btn-outline-info"onClick={() => this.handleClick('1d')}>1DAY </button>
+        <button className="btn btn-outline-info"onClick={() => this.handleClick('7d')}>7DAY </button>
+        <button className="btn btn-outline-info"onClick={() => this.handleClick('1m')}>1Month </button>
+        <button className="btn btn-outline-info"onClick={() => this.handleClick('3m')}>3Month </button>
+        <button className="btn btn-outline-info"onClick={() => this.handleClick('1y')}>1Year </button>
+        <br/>
+        <br/>
+      </div>
     );
   }
   
